ae142a8dd20df8cf0769fe48f3f89df2d2426c3d
JayeshWani7 <wanijayesh0@gmail.com>
2025-01-02 13:38:11 +0530

    Refactor Firebase configuration to use environment variables

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Replace the values below with your Firebase project settings
 const firebaseConfig = {
-  apiKey: "AIzaSyAfZt1nST5N3IU9OYYb7yImey-HEDcsOSg",
-  authDomain: "future-city-b6cb1.firebaseapp.com",
-  projectId: "future-city-b6cb1",
-  storageBucket: "future-city-b6cb1.firebasestorage.app",
-  messagingSenderId: "598712444465",
-  appId: "1:598712444465:web:0253a57fb040a4958f9710",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase App
